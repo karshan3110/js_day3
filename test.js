44b6cd6b54fba1eaf84b5878fd7d3a884151d8c6
karshan3110 <karshan3110@gmail.com>
2025-10-03 10:07:27 +0530

    second commit

--- test.js
+++ test.js
@@ -1,23 +1,31 @@
-// // myObj = { "name":"Jane", "age":18, "city":"Chicago" };
-// function fn(){
-// hi=document.getElementById("x").ariaValueMax;
-// myJSON = JSON.stringify(hi);                 // storing data
-// localStorage.setItem("testJSON", myJSON);   
-// text = localStorage.getItem("testJSON");        // retrieving data 
-// obj = JSON.parse(text);
-// document.write(obj);
+let pin = prompt("Enter your PIN : ");
+let a = confirm("DO you want to open your account !!");
+let amt = 10000;
+let f = true;
+while (f) {
+    if (a == true) {
+        z = Number(prompt("1=Deposit,\n 2=Withdraw,\n 3=Check Balance,\n 4=Exit"));
+        switch (z) {
+            case 1:
+                c = prompt("Enter Amount");
+                amt += Number(c);
+                alert("Deposit Successful ! New Balance :" + amt);
+                break;
+            case 2:
+                b = prompt("Enter Amount ?");
+                amt -= Number(b);
+                alert("Withdraw Successful ! New Balance :" + amt);
+                break;
+            case 3:
+                alert("Your Balance is :" + amt);
+                break;
+            case 4:
+                f = false;
+                break;
+            default:
+                alert("Invalid Input");
+                break
 
-// };
-
-function getInputValue() {
-    // Access the input element using its id
-    var inputValue = document.getElementById("myInput").value;
-    
-myJSON = JSON.stringify(inputValue);                 // storing data
-localStorage.setItem("testJSO", myJSON);   
-text = localStorage.getItem("testJSO");        // retrieving data 
-
-
-    // Display the value inside the <p> element with id="output"
-    document.getElementById("output").textContent = "You entered: " + text;
+        }
+    }
 }
